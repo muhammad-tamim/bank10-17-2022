90d419e41d3cf4e35bd9ff32572f3f197b31d276
Muhammad Tamim <mdt40234@gmail.com>
2022-10-17 21:29:24 +0600

    Update balance total

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
 document.getElementById('btn-deposit').addEventListener('click', function () {
     const depositInput = document.getElementById('deposit-field');
     const depositTotal = document.getElementById('deposit-total');
-    const depositAmount = parseInt(depositInput.value)
-    const depositTotalText = parseInt(depositTotal.innerText)
+    const depositAmount = parseFloat(depositInput.value)
+    const depositTotalText = parseFloat(depositTotal.innerText)
     depositTotal.innerText = depositTotalText + depositAmount;
+    // update total balance
+    const balanceTotal = document.getElementById('balance-total')
+    const previousBalanceTotal = parseFloat(balanceTotal.innerText);
+    balanceTotal.innerText = previousBalanceTotal + depositAmount;
     // console.log(depositTotalText);
     depositInput.value = "";
 
