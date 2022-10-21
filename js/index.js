ed5ddfc35641e03fa74b998a1db1563e6b7e0e84
Muhammad Tamim <mdt40234@gmail.com>
2022-10-21 17:17:14 +0600

    js code make better

--- js/index.js
+++ js/index.js
@@ -5,24 +5,21 @@ function getInputValue(inputId) {
     inputField.value = "";
     return inputAmount;
 }
+function updateTotalFeild(Amount, totalFeildId) {
+    const Total = document.getElementById(totalFeildId);
+    const TotalText = parseFloat(Total.innerText)
+    Total.innerText = TotalText + Amount;
+    return Total;
+}
 document.getElementById('btn-deposit').addEventListener('click', function () {
-    const depositTotal = document.getElementById('deposit-total');
     const depositAmount = getInputValue('deposit-field')
-
-    const depositTotalText = parseFloat(depositTotal.innerText)
-    depositTotal.innerText = depositTotalText + depositAmount;
+    const depositTotal = updateTotalFeild(depositAmount, 'deposit-total');
     // update total balance
-    const balanceTotal = document.getElementById('balance-total')
-    const previousBalanceTotal = parseFloat(balanceTotal.innerText);
-    balanceTotal.innerText = previousBalanceTotal + depositAmount;
-    // console.log(depositTotalText);
-
+    const balanceTotal = updateTotalFeild(depositAmount, 'balance-total')
 })
 document.getElementById('btn-withdraw').addEventListener('click', function () {
-    const withdrawTotal = document.getElementById('withdraw-total');
     const withdrawAmount = getInputValue('withdraw-field');
-    const prevoiusWithdrawTotal = parseFloat(withdrawTotal.innerText);
-    withdrawTotal.innerText = prevoiusWithdrawTotal + withdrawAmount;
+    const withdrowTotal = updateTotalFeild(withdrawAmount, "withdraw-total")
     const balanceTotal = document.getElementById('balance-total');
     const prevoiusBalanceTotal = parseFloat(balanceTotal.innerText)
     balanceTotal.innerText = prevoiusBalanceTotal - withdrawAmount;
