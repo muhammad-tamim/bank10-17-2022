2c15d1c40899b9d7381d8e90a836ce5dca338b97
Muhammad Tamim <mdt40234@gmail.com>
2022-10-22 16:49:54 +0600

    Handel error and adding comment's

--- js/index.js
+++ js/index.js
@@ -5,22 +5,52 @@ function getInputValue(inputId) {
     inputField.value = "";
     return inputAmount;
 }
-function updateTotalFeild(Amount, totalFeildId) {
+function getCurrentValue(totalFeildId) {
     const Total = document.getElementById(totalFeildId);
     const TotalText = parseFloat(Total.innerText)
-    Total.innerText = TotalText + Amount;
+    return TotalText;
+}
+function updateDepoWithdrowTotalBalance(Amount, totalFeildId, isAdd) {
+    const Total = document.getElementById(totalFeildId);
+    const TotalText = getCurrentValue(totalFeildId)
+    if (isAdd == true) {
+        Total.innerText = TotalText + Amount;
+    } else {
+        Total.innerText = TotalText - Amount;
+    }
     return Total;
 }
 document.getElementById('btn-deposit').addEventListener('click', function () {
+    //Find deposit-field value and convart number
     const depositAmount = getInputValue('deposit-field')
-    const depositTotal = updateTotalFeild(depositAmount, 'deposit-total');
-    // update total balance
-    const balanceTotal = updateTotalFeild(depositAmount, 'balance-total')
+    // Handel NaN error and negetive value error
+    if (depositAmount > 0) {
+        // Find deposit-total value and sum depositAmount 
+        const depositTotal = updateDepoWithdrowTotalBalance(depositAmount,
+            'deposit-total', true);
+        // Find balance-total value and sum depositAmount 
+        const balanceTotal = updateDepoWithdrowTotalBalance(depositAmount,
+            'balance-total', true)
+    } else {
+        prompt('Plz , Give me positive number.')
+    }
 })
 document.getElementById('btn-withdraw').addEventListener('click', function () {
+    //Find withdraw-field value and convart number
     const withdrawAmount = getInputValue('withdraw-field');
-    const withdrowTotal = updateTotalFeild(withdrawAmount, "withdraw-total")
-    const balanceTotal = document.getElementById('balance-total');
-    const prevoiusBalanceTotal = parseFloat(balanceTotal.innerText)
-    balanceTotal.innerText = prevoiusBalanceTotal - withdrawAmount;
+    const balanceTotal = getCurrentValue('balance-total');
+    // Handel negetive error and NaN error
+    if (balanceTotal < withdrawAmount) {
+        prompt('You have onlay balance.', balanceTotal)
+    }
+    else if (withdrawAmount > 0) {
+        // Find withdraw-total value and sum depositAmount 
+        const withdrowTotal = updateDepoWithdrowTotalBalance(withdrawAmount,
+            "withdraw-total", true)
+        // Find balance-total value and sum depositAmount 
+        const balanceTotal = updateDepoWithdrowTotalBalance(withdrawAmount,
+            'balance-total', false)
+    } else {
+        prompt('Plz , Give me positive number.')
+    }
 })
